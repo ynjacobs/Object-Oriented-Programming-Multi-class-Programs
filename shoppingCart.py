ca570a0d822363f68287ee9009b412c2422a850d
ynjacobs <ytj64611@gamil.com>
2019-05-13 15:35:29 -0400

    imports from Product class

---
+++ shoppingCart.py
@@ -0,0 +1,46 @@
+from product import Product
+
+class ShoppingCart:
+
+    def __init__(self):
+        self.products = []
+
+    def __str__(self):
+       return 'Your cart has:\n '+ '\n'.join(str(product) for product in self.products)
+        
+        
+
+    def add_to_cart(self, product):
+        self.products.append(product)
+        return "you have added {} to your cart".format(product.name)
+
+    def remove_from_cart(self, product):
+        self.products.remove(product)
+        return "you have removed {} to your cart".format(product.name)
+    
+    def total_before_tax(self):
+        total_price = 0 
+        for product in self.products:
+            total_price += product.base_price
+        return total_price
+
+    def after_tax(self):
+        total = 0 
+        for product in self.products:
+            total += product.calculate()
+        return total
+
+apple = Product('apple', 3, 0.13)
+pear = Product('pear', 2, 0.13)
+shopping_cart = ShoppingCart()
+print(shopping_cart.add_to_cart(apple))
+shopping_cart.add_to_cart(pear)
+# shopping_cart.remove_from_cart(apple)
+
+print('your total is $', shopping_cart.total_before_tax())
+print('your total after tax is $', shopping_cart.after_tax())
+
+
+
+
+print(shopping_cart)
